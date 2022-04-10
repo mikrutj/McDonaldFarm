437453fac637710673df78b054fb191ceeaf2e50
mikrutj <mikrutj@outlook.com>
2022-04-10 20:39:32 +0200

    Value check enhanced

--- McDonaldFarm/FenceCalculator.cpp
+++ McDonaldFarm/FenceCalculator.cpp
@@ -1,53 +1,61 @@
 #include <iostream>
+#include <string>
+#include <limits>
+
 #include "FenceCalculator.h"
 
 
 using namespace std;
 
-int FenceCalculator::getFence() {
-    
-    double fence;
+bool isNumber(string s) {
+    //Check if is number
+    for (int i = 0; i < s.length(); i++)
+        if (isdigit(s[i]) == false)
+            return false;
 
-    cout << "How many meters of fence you have: ";
-    cin >> fence;
-
-    //Value check
-    while (!cin.good() || (fence < 3) || (fence - (int)fence != 0))
-    {
-        //Report problem
-        if (!cin.good())
-        {
-            cout << "Wrong value. Please try again..." << endl;
-        }
-        else if (fence - (int)fence != 0)
-        {
-            if (fence > INT_MAX)
-            {
-                cout << "It is a lot of fence. Maybe something smaller?..." << endl;
-            }
-            else
-                cout << "Wrong value. Please try an integer number..." << endl;
-        }
-        else if (fence < 3)
-        {
-            cout << "We can't build anything out of it. Try to buy more..." << endl;
-        }
-        else
-            cout << "Wrong value. Please try again..." << endl;
-
-        //Clear stream
-        cin.clear();
-        cin.ignore(INT_MAX, '\n');
-
-        //Get input again
-        cout << "How many meters of fence you have: ";
-        cin >> fence;
-    }
-    return (int)fence;
+    return true;
 }
 
-unsigned long long int FenceCalculator::getArea (int f, int a)
-{
+int FenceCalculator::getFence() {
+    
+    string fence;
+
+    cout << "How many meters of fence you have: ";
+    getline(cin, fence);
+    
+    //Value check
+    while (true) {
+        if (isNumber(fence) && fence.length() <= 10 && stoll(fence) <= INT_MAX && stoll(fence) >= 3) {
+            break;
+        }
+        else {
+            //Report problem
+            if ((!isNumber(fence.substr(1)) && fence[0] == '-') || (!isNumber(fence) && fence[0] != '-')) {
+                cout << "Wrong value. Please try an integer number..." << endl;
+            }
+            else if (isNumber(fence.substr(1)) && fence[0] == '-') {
+                cout << "Of course... and pig can fly... Try to be more realistic..." << endl;
+            }
+            else if (fence.length() > 10 || stoll(fence) > INT_MAX) {
+                cout << "It is a lot of fence. Maybe something smaller?..." << endl;
+            } 
+            else if (stoll(fence) < 3) {
+                cout << "We can't build anything out of it. Try to buy more..." << endl;
+            }
+            else {
+                cout << "Wrong value. Please try again..." << endl;
+            }
+
+            cout << "How many meters of fence you have: ";
+            getline(cin, fence);
+        }
+        
+    }
+
+    return stoll(fence);
+}
+
+unsigned long long int FenceCalculator::getArea (int f, int a) {
     //Calculate area
     //----------------------------------
     //2a + b = f
@@ -58,13 +66,11 @@ unsigned long long int FenceCalculator::getArea (int f, int a)
     return (unsigned long long int)a * (f - (2 * a));//((f * a) - (2 * pow(a, 2)));
 }
 
-unsigned long long int FenceCalculator::getArea()
-{
+unsigned long long int FenceCalculator::getArea() {
     return (unsigned long long int) a * b;
 }
 
-int FenceCalculator::findA (int f)
-{
+int FenceCalculator::findA (int f) {
     //Calculations 
     //----------------------------------
     // area - A,  
@@ -75,7 +81,9 @@ int FenceCalculator::findA (int f)
     // A = a * b 
     // A(a) = a * ( f - 2*a) = f*a - 2*pow(a,2)
     // 
-    // A'(a) = f - 4*a       derivative
+    // in this case the function will have max value when its derivative is equal to zero
+    // 
+    // A'(a) = f - 4*a   
     // 
     // A'(a) = 0  <=>  a = f/4
     //----------------------------------
@@ -83,18 +91,16 @@ int FenceCalculator::findA (int f)
     double a = (f * 0.25);
     double area1, area2;
 
-    //Find a bigger area if a != integer
-    if (a - (int)a != 0)
-    {
+    //Find a bigger area if a is not integer
+    if (a - (int)a != 0) {
+
         area1 = FenceCalculator::getArea(f, ceil(a));
         area2 = FenceCalculator::getArea(f, floor(a));
 
-        if (area1 < area2)
-        {
+        if (area1 < area2) {
             return floor(a);
         }
-        else
-        {
+        else {
             return ceil(a);
         }
     }
--- McDonaldFarm/McDonaldFarm.cpp
+++ McDonaldFarm/McDonaldFarm.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <algorithm>
 #include <conio.h>
+#include <string>
 
 #include "FenceCalculator.h"
 
 
 using namespace std;
+
 
 void start() {
 
@@ -13,70 +15,63 @@ void start() {
 
     //Ask to start
     cout << "Please write \"RUN\" to start: ";
-    cin >> run;
+    getline(cin, run);
 
     //Transform input to upper
     transform(run.begin(), run.end(), run.begin(), ::toupper);
 
     //Check input
-    while (!cin.good() || (run != "RUN"))
-    {
+    while (!cin.good() || (run != "RUN")) {
         //Report problem
         cout << "Please try again..." << endl;
 
-        //Clear stream
-        cin.clear();
-        cin.ignore();
-
         //Get input again
         cout << "Please write \"RUN\" to start: ";
-        cin >> run;
+        getline(cin, run);
 
         //Transform input to upper
         transform(run.begin(), run.end(), run.begin(), ::toupper);
     }
-
     cout << "Mission: Accepted... Start the program..." << endl << endl;
 
 }
 
-
 void gameLoop() {
 
     start();
-    
+    //Set initial value
     string choice = "Y";
     bool isWrongValue = false;
 
+    
     while (true) {
 
         if (choice == "y" || choice == "Y") {
-            FenceCalculator c1;
+            FenceCalculator calc;
+            //Print value
+            cout << "you can build a fence with dimensions a = " << calc.getA()
+                << "m and b = " << calc.getB() << "m." << endl; 
+            cout << "The area will be " << calc.getArea() << "m^2." << endl;;
 
-            cout << "a = " << c1.getA() << " b = " << c1.getB() << " area = " << c1.getArea();
-
+            //Set values for play again
             choice = "";
             isWrongValue = false;
         }
-        else if (choice == "n" || choice == "N")
-        {
+        else if (choice == "n" || choice == "N") {
             break;
         }
-        else
-        {
+        else {
+            //Play again
             string message = isWrongValue ? 
                 "Wrong value! Try again!\nDo you want to calculate another fence [Y/N]: " : 
                 "Do you want to calculate another fence [Y/N]: ";
             cout << endl << message;
 
-            cin >> choice;
+            getline(cin, choice);
             isWrongValue = true;
         }
-
     }
-
 }
-
 
 
 
