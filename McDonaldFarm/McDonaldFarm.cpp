9b253d46110185522e5fe14deb89a342c28c588b
mikrutj <mikrutj@outlook.com>
2022-04-10 16:46:37 +0200

    GameLoop function init

--- McDonaldFarm/McDonaldFarm.cpp
+++ McDonaldFarm/McDonaldFarm.cpp
@@ -1,16 +1,51 @@
 #include <iostream>
+#include <algorithm>
+
 #include "FenceCalculator.h"
 #include "Fence.h"
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello World!\n";
 
-    FenceCalculator c1;
-    Fence c2;
+void gameLoop() {
+
+    string run;
+
+    //Ask to start
+    cout << "Please write \"RUN\" to start: ";
+    cin >> run;
+
+    //Transform input to upper
+    transform(run.begin(), run.end(), run.begin(), ::toupper);
+
+    //Check input
+    while (!cin.good() || (run != "RUN"))
+    {
+        //Report problem
+        cout << "Please try again..." << endl;
+
+        //Clear stream
+        cin.clear();
+        cin.ignore();
+
+        //Get input again
+        cout << "Please write \"RUN\" to start: ";
+        cin >> run;
+
+        //Transform input to upper
+        transform(run.begin(), run.end(), run.begin(), ::toupper);
+    }
+
+    cout << "Mission: Accepted... Start the program..." << endl;
+
+}
+
+
+int main() {
+
+    gameLoop();
 
     return 0;
 }
+
 
